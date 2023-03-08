c6f02a9f51ac38f49a01a426922b17295d93dbea
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-08 15:32:26 +0300

    Fix Linter error

--- morse.rb
+++ morse.rb
@@ -1,50 +1,50 @@
-def decode_char (char)
-keys = {
-    'A'=>'.-',
-    'B'=>'-...',
-    'C'=>'-.-.',
-    'D'=>'-..',
-    'E'=>'.',
-    'F'=>'..-.',
-    'G'=>'--.',
-    'H'=>'....',
-    'I'=>'..',
-    'J'=>'.---',
-    'K'=>'-.-',
-    'L'=>'.-..',
-    'M'=>'--',
-    'N'=>'-.',
-    'O'=>'---',
-    'P'=>'.--.',
-    'Q'=>'--.-',
-    'R'=>'.-.',
-    'S'=>'...',
-    'T'=>'-',
-    'U'=>'..-',
-    'V'=>'...-',
-    'W'=>'.--',
-    'X'=>'-..-',
-    'Y'=>'-.--',
-    'Z'=>'--..'
-}
+def decode_char(char)
+  keys = {
+    'A' => '.-',
+    'B' => '-...',
+    'C' => '-.-.',
+    'D' => '-..',
+    'E' => '.',
+    'F' => '..-.',
+    'G' => '--.',
+    'H' => '....',
+    'I' => '..',
+    'J' => '.---',
+    'K' => '-.-',
+    'L' => '.-..',
+    'M' => '--',
+    'N' => '-.',
+    'O' => '---',
+    'P' => '.--.',
+    'Q' => '--.-',
+    'R' => '.-.',
+    'S' => '...',
+    'T' => '-',
+    'U' => '..-',
+    'V' => '...-',
+    'W' => '.--',
+    'X' => '-..-',
+    'Y' => '-.--',
+    'Z' => '--..'
+  }
 
-return keys.key(char)
+  keys.key(char)
 end
 
-def decode_word (word)
-arr_word = word.split
-result = ''
-arr_word.each do |ch|
+def decode_word(word)
+  arr_word = word.split
+  result = ''
+  arr_word.each do |ch|
     result += decode_char(ch)
-end
-return result
+  end
+  result
 end
 
-def decode (words)
-array_words = words.split('   ')
-arr = []
-array_words.each do |word|
+def decode(words)
+  array_words = words.split('   ')
+  arr = []
+  array_words.each do |word|
     arr.push(decode_word(word))
-end
-return arr.join(' ')
+  end
+  arr.join(' ')
 end
