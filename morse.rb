7ec526123130f92eb504ecbdb69a0afb2ed440b8
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-08 14:22:29 +0300

    Create a method to decode an entire word in Morse

--- morse.rb
+++ morse.rb
@@ -27,3 +27,15 @@ keys = {
     'Y'=>'-.--',
     'Z'=>'--..'
 }
+
+return keys.key(char)
+end
+
+def decode_word (word)
+arr_word = word.split
+result = ''
+arr_word.each do |ch|
+    result += decode_char(ch)
+end
+return result
+end
